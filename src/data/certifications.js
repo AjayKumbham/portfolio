0d935e48c322421ec4fdc7676ef83281d2d5ae6d
AjayKumbham <ajaygoud.kumbham@gmail.com>
2025-07-02 18:23:31 +0530

    add certifications

--- src/data/certifications.js
+++ src/data/certifications.js
@@ -22,6 +22,16 @@ export const certificationsData = [
   },
   {
     id: 3,
+    certificateTitle: "Career Essentials in Data Analysis",
+    issuingOrganization: "Microsoft & LinkedIn Learning",
+    dateIssued: "Jul 2025",
+    verificationLink: "https://www.linkedin.com/learning/certificates/b31e3ed02330d14575a98cf2593cf049e0d2a848e9058df97283e8432b3eca9f",
+    credentialId: null,
+    skills: [],
+    covered: null
+  },
+  {
+    id: 4,
     certificateTitle: "Software Engineering Job Simulation",
     issuingOrganization: "JPMorgan Chase & Co. (via Forage)",
     dateIssued: "Jun 2025",
@@ -31,7 +41,7 @@ export const certificationsData = [
     covered: "Project Setup, Kafka Integration, H2 Integration, REST API Integration, REST API Controller"
   },
   {
-    id: 4,
+    id: 5,
     certificateTitle: "GenAI Powered Data Analytics Job Simulation",
     issuingOrganization: "Tata (via Forage)",
     dateIssued: "Jun 2025",
@@ -41,7 +51,7 @@ export const certificationsData = [
     covered: "Exploratory Data Analysis, Risk Profiling, Predicting Delinquency with AI, Business Storytelling, AI-Driven Strategy Implementation"
   },
   {
-    id: 5,
+    id: 6,
     certificateTitle: "Frontend Developer (React)",
     issuingOrganization: "HackerRank",
     dateIssued: "Jun 2025",
@@ -51,11 +61,11 @@ export const certificationsData = [
     covered: null
   },
   {
-    id: 6,
+    id: 7,
     certificateTitle: "Python Essentials 1",
     issuingOrganization: "Cisco Networking Academy & OpenEDG Python Institute",
     dateIssued: "Jun 2025",
-    verificationLink: null,
+    verificationLink: "https://www.credly.com/badges/f46fd8fc-31b2-48b5-bf90-d4189ed25a1c/public_url",
     credentialId: null,
     skills: [],
     covered: "Verification QR available on certificate"
